5e8d87f55148d83b244b196467f7b3eb2a376850
chris <chris@chris-wk.(none)>
2014-01-08 17:50:17 +0100

    import Base and include operators

--- src/TimeData.jl
+++ src/TimeData.jl
@@ -7,11 +7,15 @@ using DataFrames
 using DataArrays
 using Datetime
 
+importall Base
+importall Stats
+
 export #
 str,
 TimeNum
 
 include("constraints.jl")
 include("timenum.jl")
+include("operators.jl")
 
 end
