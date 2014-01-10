956bcd02e788af71f104f6f5e75abeff94c1e3b6
chris <groll.christian.edu@gmail.com>
2014-01-10 14:41:26 +0100

    new tests for constraints of Timematr also

--- test/constraints.jl
+++ test/constraints.jl
@@ -3,6 +3,7 @@ module TestConstraints
 using Base.Test
 using DataArrays
 using DataFrames
+using Datetime
 ## using TimeData
 
 ## define macro for expressions that return nothing 
@@ -10,28 +11,49 @@ macro returnsNothing(ex)
     :(isa($ex, Nothing))
 end
 
-validDates = DataArray(today())
-## @test isa(chkDates(validDates), Nothing)
-@test @returnsNothing chkDates(validDates)
+##############
+## chkDates ##
+##############
 
+## test valid dates
+validDates = DataArray(today())
+@test @returnsNothing TimeData.chkDates(validDates)
+
+## test invalid dates
 invalidDates = [1 2 3]
-@test_throws chkDates(invalidDates)
+@test_throws TimeData.chkDates(invalidDates)
 
 ## dates must be array!
 invalidDates = today()
-@test_throws chkDates(invalidDates)
+@test_throws TimeData.chkDates(invalidDates)
 
-validDf = DataFrame(quote
+#########################
+## chkNum and chkNumDf ##
+#########################
+
+## numeric DataFrame with NAs
+naDf = DataFrame(quote
     a = [1, 2, 3]
     b = [4, 5, 6]
     end)
-## @test isa(chkNumDf(validDf), Nothing)
-@test @returnsNothing chkNumDf(validDf)
+naDf[1, 2] = NA
+naDf[3, 1] = NA
+@test @returnsNothing TimeData.chkNumDf(naDf)
+@test_throws TimeData.chkNum(naDf)
 
+numDf = DataFrame(quote
+    a = [1, 2, 3]
+    b = [4, 5, 6]
+    end)
+@test @returnsNothing TimeData.chkNumDf(numDf)
+@test @returnsNothing TimeData.chkNum(numDf)
+
+## invalid DataFrame with strings
 invalidDf = DataFrame(quote
     a = [1, 2, 3]
     b = ["hello", "world", "hello"]
     end)
-@test_throws chkNumDf(invalidDates)
+@test_throws TimeData.chkNumDf(invalidDf)
+@test_throws TimeData.chkNum(invalidDf)
 
 end
