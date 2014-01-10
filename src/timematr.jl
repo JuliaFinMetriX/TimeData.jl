e67db9e1afc8ad8a719958da5f9b83c72d1883e2
chris <groll.christian.edu@gmail.com>
2014-01-10 14:44:08 +0100

    conversions added, old code from Timenum inherited -> Timematr is new Timenum

--- src/timematr.jl
+++ src/timematr.jl
@@ -1,4 +1,4 @@
-type Timematr <: AbstractTimeData
+type Timematr <: AbstractTimenum
     vals::DataFrame
     dates::DataArray
 
@@ -11,3 +11,59 @@ type Timematr <: AbstractTimeData
         return new(vals, dates)
     end
 end
+
+#################
+## Conversions ##
+#################
+
+## conversion upwards: always works
+function convert(Timedata, tm::Timematr)
+    Timedata(tm.vals, tm.dates)
+end
+
+## conversion upwards: always works
+function convert(Timenum, tm::Timematr)
+    Timenum(tm.vals, tm.dates)
+end
+
+
+#############################
+## get numeric values only ##
+#############################
+
+## possible without NAs
+function core(tm::Timematr)
+    return matrix(tm.vals)
+end
+
+#######################
+## Timematr get mean ##
+#######################
+
+import Base.mean
+function mean(tm::Timematr, dim::Int = 1)
+    ## output: DataFrame, since date dimension is lost
+    if dim == 2
+        error("For rowwise mean use rowmeans function")
+    end
+    meanVals = mean(core(tm), dim)
+    means = DataFrame(meanVals, vars(tm))
+end
+
+function rowmeans(tm::Timematr)
+    ## output: Timematr
+    meanVals = mean(core(tm), 2)
+    means = Timematr(meanVals, dates(tm))
+end
+
+#########################
+## Timematr covariance ##
+#########################
+
+import Base.cov
+function cov(tm::Timematr)
+    ## output: DataFrame
+    covDf = DataFrame(cov(core(vals)), vars(tm.vals))
+    return covDf
+end
+
