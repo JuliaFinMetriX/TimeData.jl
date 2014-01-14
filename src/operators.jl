8f6b0605a1a96ee81745d67069ddd6996e2a4198
chris <groll.christian.edu@gmail.com>
2014-01-14 22:30:11 +0100

    method signature only once in comments

--- src/operators.jl
+++ src/operators.jl
@@ -2,22 +2,6 @@
 ## type preserving symmetric operators and functions ##
 #######################################################
 
-## symmetric operators and functions with method signatures: 
-## f(b::NAtype, inst::NewType)
-## f(inst::NewType, b::NAtype)
-## f(inst::NewType, b::NewType)
-## f(inst::NewType, b::Union(String,Number))
-## f(b::Union(String,Number), inst::NewType)
-
-## elementwise: * and / excluded -> should mean matrix multiplication
-## for Timenum
-const element_wise_operators = [:(+), :(.+), :(-), :(.-), :(*), :(.*),
-                                     :(/), :(./), :(.^)]
-const element_wise_operators_ext = [:(div), :(mod), :(fld), :(rem)]
-
-pres_msSymmetric_functions = [element_wise_operators,
-                              element_wise_operators_ext]
-
 ## type preserving symmetric operations and functions
 ##
 ## f(b::NAtype, inst::NewType) = NewType(f(b, inst.vals), dates(inst)) 
@@ -44,8 +28,16 @@ macro pres_msSymmetric(f, myType)
     end)
 end
 
-importall Base
+## elementwise: * and / excluded -> should mean matrix multiplication
+## for Timenum
+const element_wise_operators = [:(+), :(.+), :(-), :(.-), :(*), :(.*),
+                                     :(/), :(./), :(.^)]
+const element_wise_operators_ext = [:(div), :(mod), :(fld), :(rem)]
 
+pres_msSymmetric_functions = [element_wise_operators,
+                              element_wise_operators_ext]
+
+importall Base
 for t = (:Timedata, :Timenum, :Timematr)
     for f in pres_msSymmetric_functions
         ## print(macroexpand(:(@swap_type($f, $t))))
@@ -61,7 +53,14 @@ end
 #################################################
 
 ## type preserving functions with method signature:
-## f(inst::NewType)
+## f(inst::NewType) = NewType(f(inst.vals), dates(inst))
+macro pres_msUnitary(f, myType)
+    esc(quote
+        $(f)(inst::$(myType)) =
+            $(myType)($(f)(inst.vals), dates(inst))
+    end)
+end
+
 
 const unary_operators = [:(+), :(-)]    # [:(*), :(/)]
 
@@ -75,13 +74,6 @@ const mathematical_functions = [:abs, :sign,
 
 pres_msUnitary_functions = [unary_operators, mathematical_functions]
 
-## $(f)(td::Timedata) = Timedata($(f)(td.vals, dates(td))
-macro pres_msUnitary(f, myType)
-    esc(quote
-        $(f)(inst::$(myType)) =
-            $(myType)($(f)(inst.vals), dates(inst))
-    end)
-end
 
 
 
@@ -97,11 +89,29 @@ end
 ######################################################
 
 ## symmetric operators and functions with method signatures: 
-## f(b::NAtype, inst::NewType)
-## f(inst::NewType, b::NAtype)
-## f(inst::NewType, b::NewType)
-## f(inst::NewType, b::Union(String,Number))
-## f(b::Union(String,Number), inst::NewType)
+## f(b::NAtype, inst::NewType) = NewType(f(b, inst.vals), dates(inst))
+## f(inst::NewType, b::NAtype) = NewType(f(inst.vals, b), dates(inst))
+## f(inst::NewType, inst2::NewType) = NewType(f(inst.vals, inst2.vals), dates(inst))
+## f(inst::NewType, b::Union(String,Number)) = NewType(f(inst.vals, b), dates(inst))
+## f(b::Union(String,Number), inst::NewType) = NewType(f(b, inst.vals), dates(inst))
+macro nonpres_msSymmetric(f)
+    esc(quote
+        $(f)(b::NAtype,inst::AbstractTimedata) =
+            Timedata($(f)(b,inst.val), dates(inst))
+
+        $(f)(inst::AbstractTimedata,b::NAtype) =
+            Timedata($(f)(inst.val,b), dates(inst))
+        
+        $(f)(inst::AbstractTimedata, inst2::AbstractTimedata) =
+            Timedata($(f)(inst.vals, inst2.vals), dates(inst))
+
+        $(f)(inst::AbstractTimedata,b::Union(String,Number)) =
+            Timedata($(f)(inst.val,b), dates(inst))
+        
+        $(f)(b::Union(String,Number),inst::AbstractTimedata) =
+            Timedata($(f)(b,inst.val), dates(inst))
+    end)
+end
 
 const element_wise_comparisons = [:(.==), :(.!=), :(.>), :(.>=),
                                      :(.<), :(.<=)]
@@ -112,24 +122,6 @@ nonpres_msSymmetric_functions = [element_wise_comparisons,
                            element_wise_logicals]
 
 
-macro nonpres_msSymmetric(f)
-    esc(quote
-        $(f)(inst::AbstractTimedata, inst2::AbstractTimedata) =
-            Timedata($(f)(inst.vals, inst2.vals), dates(inst))
-
-        $(f)(inst::AbstractTimedata,b::Union(String,Number)) =
-            Timedata($(f)(inst.val,b), dates(inst))
-        
-        $(f)(b::Union(String,Number),inst::AbstractTimedata) =
-            Timedata($(f)(b,inst.val), dates(inst))
-
-        $(f)(inst::AbstractTimedata,b::NAtype) =
-            Timedata($(f)(inst.val,b), dates(inst))
-        
-        $(f)(b::NAtype,inst::AbstractTimedata) =
-            Timedata($(f)(b,inst.val), dates(inst))
-    end)
-end
 
 for f in nonpres_msSymmetric_functions
     eval(macroexpand(:(@nonpres_msSymmetric($f))))                    
@@ -140,14 +132,7 @@ end
 ## type preserving functions, single or extra argument ##
 #########################################################
 
-## symmetric operators and functions with method signatures: 
-## f(inst::NewType)
-## f(inst::NewType, i::Integer)
-
-const rounding_operators = [:round, :ceil, :floor, :trunc]
-
-pres_msSingle_or_extra_functions = rounding_operators
-
+## symmetric operators and functions with method signatures:
 ## f(td::NewType) = NewType(f(td.vals), dates(td))
 ## f(td::NewType, i::Integer) = NewType(f(td.vals, i), dates(td))
 macro pres_msSingle_or_extra(f, myType)
@@ -156,6 +141,10 @@ macro pres_msSingle_or_extra(f, myType)
             $(myType)($(f)(inst.vals, args...), dates(inst))
     end)
 end
+
+const rounding_operators = [:round, :ceil, :floor, :trunc]
+
+pres_msSingle_or_extra_functions = rounding_operators
 
 for t = (:Timedata, :Timenum, :Timematr)
     for f in pres_msSingle_or_extra_functions
