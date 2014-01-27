7868000c9c8bdcb1024b31e4fe3ab9449a8cd4ad
Christian Groll <groll.christian.edu@gmail.com>
2014-01-27 11:42:25 +0100

    fixed timenum to timematr

--- test/timematr.jl
+++ test/timematr.jl
@@ -1,17 +1,29 @@
-## using Base.Test
+module TestConstraints
+
+using Base.Test
+using DataArrays
+using DataFrames
+using Datetime
+using TimeData
+
+println("\n Running timematr tests\n")
 
 #######################################
 ## reconstruction from core elements ##
 #######################################
+
+## create instance
+vals = rand(30, 4)
+dats = [date(2013, 7, ii) for ii=1:30]
+nams = ["A", "B", "C", "D"]
 
 ## reconstructing Timematr instance from core elements
 tm = TimeData.Timematr(vals, nams, dats)
 vals2 = TimeData.core(tm)
 nams2 = TimeData.names(tm)
 dats2 = TimeData.dates(tm)
-tm2 = TimeData.Timenum(vals2, nams2, dats2)
+tm2 = TimeData.Timematr(vals2, nams2, dats2)
 @test TimeData.isequal(tm, tm2)
-
 
 ## core function should not return Array{Real, 2}
 vals1 = [4000, 4001, 4002]
@@ -19,3 +31,5 @@ vals2 = [1., 2., 3.]
 df = DataFrame(a = vals1, b = vals2)
 tm = TimeData.Timematr(df)
 @test isa(TimeData.core(tm), Array{Float64})
+
+end
