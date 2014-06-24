2f37668cb86a0acdb966c6149bbf0646d3502575
Christian Groll <groll.christian.edu@gmail.com>
2014-06-24 18:00:18 +0200

    fixed second getindex single column mistake

--- src/getindex.jl
+++ src/getindex.jl
@@ -74,7 +74,7 @@ for t = (:Timedata, :Timenum, :Timematr, :Timecop)
             name = names(td)[selected_column] # ASCIIString
             
             ## create respective dataframe
-            valsDf = convert(DataFrame, array(vals))
+            valsDf = convert(DataFrame, vals)
             names!(valsDf, [name])           # names must be given as
             # array 
             
