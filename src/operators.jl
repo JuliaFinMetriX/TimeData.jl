035f2fe69e1d1bf1147d4ff4be5f1a94af1016ba
Christian Groll <groll.christian.edu@gmail.com>
2014-07-17 12:56:38 +0200

    Timematr operators moved

--- src/operators.jl
+++ src/operators.jl
@@ -2,49 +2,43 @@
 ## type preserving symmetric operators and functions ##
 #######################################################
 
-## type preserving symmetric operations and functions
-##
-## f(b::NAtype, inst::NewType) = NewType(f(b, inst.vals), idx(inst)) 
-## f(inst::NewType, b::NAtype) = NewType(f(inst.vals, b), idx(inst))
-## f(inst::NewType, inst2::NewType) = NewType(f(inst1.vals, inst2.vals), idx(inst))
-## f(inst::NewType, b::Union(String,Number)) = NewType(f(inst.vals, b), idx(inst))
-## f(b::Union(String,Number), inst::NewType) = NewType(f(b, inst.vals), idx(inst))
-macro pres_msSymmetric(f, myType)
-    esc(quote
-        $(f)(inst::$(myType),b::NAtype) =
-            $(myType)($(f)(inst.vals,b), idx(inst))
-        
-        $(f)(b::NAtype,inst::$(myType)) =
-            $(myType)($(f)(b,inst.vals), idx(inst))
+## function headers
+## f(inst::NewType, inst2::NewType)
+## f(inst::NewType, b::Number)
+## f(b::Number, inst::NewType)
 
-        $(f)(inst::$(myType), inst2::$(myType)) =
-            $(myType)($(f)(inst.vals, inst2.vals), idx(inst))
+## :(+) and :(-) removed -> follow-up costs
 
-        $(f)(inst::$(myType),b::Union(String,Number)) =
-            $(myType)($(f)(inst.vals,b), idx(inst))
-        
-        $(f)(b::Union(String,Number),inst::$(myType)) =
-            $(myType)($(f)(b,inst.vals), idx(inst))
-    end)
-end
-
-## elementwise: * and / excluded -> should mean matrix multiplication
-## for Timenum
-const element_wise_operators = [:(+), :(.+), :(-), :(.-), :(*), :(.*),
+const element_wise_operators = [:(.+), :(.-), :(*), :(.*),
                                      :(/), :(./), :(.^)]
 const element_wise_operators_ext = [:(div), :(mod), :(fld), :(rem)]
 
 pres_msSymmetric_functions = [element_wise_operators,
                               element_wise_operators_ext]
 
+macro pres_msSymmetric(f, myType)
+    esc(quote
+        $(f)(inst::$(myType), inst2::$(myType)) =
+            $(myType)($(f)(core(inst), core(inst2)),
+                      names(inst), idx(inst))
+
+        $(f)(inst::$(myType),b::Union(String,Number)) =
+            $(myType)($(f)(core(inst), b), names(inst), idx(inst)) 
+        
+        $(f)(b::Union(String,Number),inst::$(myType)) =
+            $(myType)($(f)(b,core(inst)), names(inst), idx(inst))
+    end)
+end
+
 importall Base
-for t = (:Timedata, :Timenum, :Timematr, :Timecop)
+for t = (:Timematr, :Timecop)
     for f in pres_msSymmetric_functions
         ## print(macroexpand(:(@swap_type($f, $t))))
         eval(macroexpand(:(@pres_msSymmetric($f, $t))))        
         ## @eval begin
         ##     @swap_type $(f) $(t)
         ## end
+
     end
 end
 
@@ -52,16 +46,7 @@ end
 ## type preserving functions without arguments ##
 #################################################
 
-## type preserving functions with method signature:
-## f(inst::NewType) = NewType(f(inst.vals), idx(inst))
-macro pres_msUnitary(f, myType)
-    esc(quote
-        $(f)(inst::$(myType)) =
-            $(myType)($(f)(inst.vals), idx(inst))
-    end)
-end
-
-
+## f(inst::NewType)
 const unary_operators = [:(+), :(-)]    # [:(*), :(/)]
 
 const mathematical_functions = [:abs, :sign,
@@ -74,7 +59,14 @@ const mathematical_functions = [:abs, :sign,
 
 pres_msUnitary_functions = [unary_operators, mathematical_functions]
 
-for t = (:Timedata, :Timenum, :Timematr, :Timecop)
+macro pres_msUnitary(f, myType)
+    esc(quote
+        $(f)(inst::$(myType)) =
+            $(myType)($(f)(core(inst)), names(inst), idx(inst))
+    end)
+end
+
+for t = (:Timematr, :Timecop)
     for f in pres_msUnitary_functions
         ## @timedata_unary f t
         eval(macroexpand(:(@pres_msUnitary($f, $t))))                
@@ -85,40 +77,30 @@ end
 ## non-preserving symmetric functions and operators ##
 ######################################################
 
-## symmetric operators and functions with method signatures: 
-## f(b::NAtype, inst::NewType) = NewType(f(b, inst.vals), idx(inst))
-## f(inst::NewType, b::NAtype) = NewType(f(inst.vals, b), idx(inst))
-## f(inst::NewType, inst2::NewType) = NewType(f(inst.vals, inst2.vals), idx(inst))
-## f(inst::NewType, b::Union(String,Number)) = NewType(f(inst.vals, b), idx(inst))
-## f(b::Union(String,Number), inst::NewType) = NewType(f(b, inst.vals), idx(inst))
-macro nonpres_msSymmetric(f)
-    esc(quote
-        $(f)(b::NAtype,inst::AbstractTimedata) =
-            Timedata($(f)(b,inst.vals), idx(inst))
-
-        $(f)(inst::AbstractTimedata,b::NAtype) =
-            Timedata($(f)(inst.vals,b), idx(inst))
-        
-        $(f)(inst::AbstractTimedata, inst2::AbstractTimedata) =
-            Timedata($(f)(inst.vals, inst2.vals), idx(inst))
-
-        $(f)(inst::AbstractTimedata,b::Union(String,Number)) =
-            Timedata($(f)(inst.vals,b), idx(inst))
-        
-        $(f)(b::Union(String,Number),inst::AbstractTimedata) =
-            Timedata($(f)(b,inst.vals), idx(inst))
-    end)
-end
+## f(inst::NewType, inst2::NewType)
+## f(inst::NewType, b::Number)
+## f(b::Number, inst::NewType)
 
 const element_wise_comparisons = [:(.==), :(.!=), :(.>), :(.>=),
                                      :(.<), :(.<=)]
 
-const element_wise_logicals = [:(&), :(|), :($)]
+nonpres_msSymmetric_functions = [element_wise_comparisons]
 
-nonpres_msSymmetric_functions = [element_wise_comparisons,
-                           element_wise_logicals]
+macro nonpres_msSymmetric(f)
+    esc(quote
+        $(f)(inst::AbstractTimedata, inst2::AbstractTimedata) =
+            Timedata(convert(Array{Bool, 2}, $(f)(core(inst), core(inst2))),
+                     names(inst), idx(inst)) 
 
-
+        $(f)(inst::AbstractTimedata,b::Union(String,Number)) =
+            Timedata(convert(Array{Bool, 2}, $(f)(core(inst),b)),
+                     names(inst), idx(inst)) 
+        
+        $(f)(b::Union(String,Number),inst::AbstractTimedata) =
+            Timedata(convert(Array{Bool, 2}, $(f)(b,core(inst))),
+                     names(inst), idx(inst))
+    end)
+end
 
 for f in nonpres_msSymmetric_functions
     eval(macroexpand(:(@nonpres_msSymmetric($f))))                    
@@ -129,47 +111,43 @@ end
 ## type preserving functions, single or extra argument ##
 #########################################################
 
-## symmetric operators and functions with method signatures:
-## f(td::NewType) = NewType(f(td.vals), idx(td))
-## f(td::NewType, i::Integer) = NewType(f(td.vals, i), idx(td))
-macro pres_msSingle_or_extra(f, myType)
-    esc(quote
-        $(f)(inst::$(myType)) =
-            $(myType)($(f)(inst.vals), idx(inst))
-        $(f)(inst::$(myType), i::Integer) =
-            $(myType)($(f)(inst.vals, i), idx(inst))
-    end)
-end
+## f(td::NewType)
+## f(td::NewType, i::Integer)
 
 const rounding_operators = [:round, :ceil, :floor, :trunc]
 
 pres_msSingle_or_extra_functions = rounding_operators
 
-for t = (:Timedata, :Timenum, :Timematr, :Timecop)
+macro pres_msSingle_or_extra(f, myType)
+    esc(quote
+        $(f)(inst::$(myType)) =
+            $(myType)($(f)(core(inst)), names(inst), idx(inst))
+        $(f)(inst::$(myType), i::Integer) =
+            $(myType)($(f)(core(inst), i), names(inst), idx(inst))
+    end)
+end
+
+for t = (:Timematr, :Timecop)
     for f in pres_msSingle_or_extra_functions
         ## @varargs_type f t
         eval(macroexpand(:(@pres_msSingle_or_extra($f, $t))))
     end
 end
 
-###########################
-## statistical functions ##
-###########################
+######################################################
+## non-preserving comparison operators for timedata ##
+######################################################
 
-## some need additional input for dimension
-## const stat_functions = [:(mean), :(cov)]
-## const unary_vector_operators = [:minimum, :maximum, :prod, :sum, :mean, :median, :std,
-##                                 :var, :mad, :norm, :skewness, :kurtosis]
-## const pairwise_vector_operators = [:diff, :reldiff]#, :percent_change]
-## const cumulative_vector_operators = [:cumprod, :cumsum, :cumsum_kbn, :cummin, :cummax]
-## const rowwise_operators = [:rowmins, :rowmaxs, :rowprods, :rowsums,
-##                            :rowmeans, :rowmedians, :rowstds, :rowvars,
-##                            :rowffts, :rownorms]
-## const columnar_operators = [:colmins, :colmaxs, :colprods, :colsums,
-##                             :colmeans, :colmedians, :colstds, :colvars,
-##                             :colffts, :colnorms]
-## const boolean_operators = [:any, :all]
+const element_wise_logicals = [:(&), :(|), :($)]
 
+macro time_data_comparison(f)
+    esc(quote
+        $(f)(td1::Timedata, td2::Timedata) =
+            Timedata($(f)(array(td1.vals), array(td2.vals)),
+                     names(td1), idx(td2))
+    end)
+end
 
-## still missing:
-## assign, ref, sub, copy, %, merge, flipud, delete!, deepcopy, similar, empty!
+for f in element_wise_logicals
+    eval(macroexpand(:(@time_data_comparison($f))))
+end
