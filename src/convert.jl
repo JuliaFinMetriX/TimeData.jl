e407440293e902887c57ecdf283ff880e35f26bf
Christian Groll <groll.christian.edu@gmail.com>
2014-08-06 07:57:36 +0200

    added function to convert DataFrame to AbstractTimedata

--- src/convert.jl
+++ src/convert.jl
@@ -48,3 +48,54 @@ function convert(::Type{Timematr}, td::Timedata)
     Timematr(td.vals, td.idx)
 end
 
+################
+## DataFrames ##
+################
+
+## convert DataFrame with dates column (as String) to TimeData object 
+
+function convert(::Type{AbstractTimedata}, df::DataFrame)
+    # find columns that have been parsed as Strings by readtable
+    col_to_test = Array(Symbol, 0)
+
+    nCols = size(df, 2)
+    for ii=1:nCols
+        isa(df[1, ii], String)?
+        push!(col_to_test, names(df)[ii]):
+        nothing
+    end
+
+    # test each column's data to see if Datetime will parse it
+    col_that_pass = Array(Symbol, 0)
+
+    for colname in col_to_test
+        d = match(r"[-|\s|\/|.]", df[1, colname])
+        d !== nothing? (bar = split(df[1, colname], d.match)): (bar = [])
+        if length(bar) == 3
+            push!(col_that_pass, colname)
+        end
+    end
+
+    # parse first column that passes the Datetime regex test
+    idx = Date{ISOCalendar}[date(d) for d in df[col_that_pass[1]]] # without
+                                        # Date it would fail chkIdx
+                                        # in constructor
+    
+    delete!(df, [col_that_pass[1]])
+
+    ## try whether DataFrame fits subtypes
+    try
+        td = Timematr(df, idx)
+        return td        
+    catch
+        try
+            td = Timenum(df, idx)
+            return td
+        catch
+            td = Timedata(df, idx)
+            return td
+        end
+    end
+
+    return td
+end
--- src/io.jl
+++ src/io.jl
@@ -2,52 +2,18 @@
 ## read and write to disk ##
 ############################
 
+function readTimedata(io::IO)
+
+    df = readtable(io)
+    td = convert(AbstractTimedata, df)
+end
+
+
+
 function readTimedata(filename::String)
 
     df = readtable(filename, nastrings=[".", "", "NA"])
-    
-    # find columns that have been parsed as Strings by readtable
-    col_to_test = Array(Symbol, 0)
-
-    nCols = size(df, 2)
-    for ii=1:nCols
-        typeof(df[1, ii]) == UTF8String?
-        push!(col_to_test, names(df)[ii]):
-        nothing
-    end
-
-    # test each column's data to see if Datetime will parse it
-    col_that_pass = Array(Symbol, 0)
-
-    for colname in col_to_test
-        d = match(r"[-|\s|\/|.]", df[1, colname])
-        d !== nothing? (bar = split(df[1, colname], d.match)): (bar = [])
-        if length(bar) == 3
-            push!(col_that_pass, colname)
-        end
-    end
-
-    # parse first column that passes the Datetime regex test
-    idx = Date{ISOCalendar}[date(d) for d in
-                                 df[col_that_pass[1]]] # without
-                                        # Date it would fail chkIdx
-                                        # in constructor
-    
-    delete!(df, [col_that_pass[1]])
-
-    ## try whether DataFrame fits subtypes
-    try
-        td = Timematr(df, idx)
-        return td        
-    catch
-        try
-            td = Timenum(df, idx)
-            return td
-        catch
-            td = Timedata(df, idx)
-            return td
-        end
-    end
+    td = convert(AbstractTimedata, df)
 end
 
 function writeTimedata(filename::String, td::AbstractTimedata)
